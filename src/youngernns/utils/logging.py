6e6996bbdce5b53ffb48d26a5cffa2cf1a5b0874
William Marstrand <wdmdevelop@gmail.com>
2024-11-13 12:59:27 +0100

    update logging

--- src/youngernns/utils/logging.py
+++ src/youngernns/utils/logging.py
@@ -3,22 +3,6 @@
 import logging
 import os
 
-# Optionally, specify the directory where log files will be stored
-LOG_DIR = os.path.join(os.path.dirname(__file__), '..', '..', '..', 'output', 'logging')
-os.makedirs(LOG_DIR, exist_ok=True)
-
-# Create handlers
-info_handler = logging.FileHandler(os.path.join(LOG_DIR, 'info.log'))
-info_handler.setLevel(logging.INFO)
-
-error_handler = logging.FileHandler(os.path.join(LOG_DIR, 'error.log'))
-error_handler.setLevel(logging.ERROR)
-
-formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-)
-info_handler.setFormatter(formatter)
-error_handler.setFormatter(formatter)
 
 # Define a custom filter to exclude ERROR and above from the info handler
 class MaxLevelFilter(logging.Filter):
@@ -30,10 +14,8 @@ class MaxLevelFilter(logging.Filter):
         # Allow records that are less severe than the exclusive_max_level
         return record.levelno < self.exclusive_max_level
 
-# Add the filter to the info handler
-info_handler.addFilter(MaxLevelFilter(logging.ERROR))
 
-def get_logger(name=None):
+def get_logger(log_path_prefix, name=None):
     """
     Returns a logger with the specified name, configured to log INFO and ERROR levels
     to separate files.
@@ -41,6 +23,26 @@ def get_logger(name=None):
     :param name: Name of the logger (usually __name__)
     :return: Configured logger
     """
+    # Optionally, specify the directory where log files will be stored
+    log_dir = os.path.join(os.path.dirname(__file__), '..', '..', '..', 'output', 'logging')
+    os.makedirs(log_dir, exist_ok=True)
+
+    # Create handlers
+    info_handler = logging.FileHandler(os.path.join(log_dir, f'{log_path_prefix}_info.log'))
+    info_handler.setLevel(logging.INFO)
+    
+    error_handler = logging.FileHandler(os.path.join(log_dir, f'{log_path_prefix}_error.log'))
+    error_handler.setLevel(logging.ERROR)
+    
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+    info_handler.setFormatter(formatter)
+    error_handler.setFormatter(formatter)
+    
+    # Add the filter to the info handler
+    info_handler.addFilter(MaxLevelFilter(logging.ERROR))
+
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)  # Set the logger to the lowest level
 
@@ -49,5 +51,5 @@ def get_logger(name=None):
         logger.addHandler(info_handler)
         logger.addHandler(error_handler)
         logger.propagate = False  # Prevent messages from propagating to the root logger
-
+        
     return logger
