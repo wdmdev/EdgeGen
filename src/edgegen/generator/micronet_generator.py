121e871418a2fe80f0a801e65846dc6cdacf25a4
William Marstrand <wdmdevelop@gmail.com>
2024-12-02 14:49:50 +0100

    add micronet todo

---
+++ src/edgegen/generator/micronet_generator.py
@@ -0,0 +1,6 @@
+from edgegen.generator import ArchitectureGenerator
+
+#TODO - Implement MicroNetGenerator
+class MicroNetGenerator(ArchitectureGenerator):
+    def __init__(self) -> None:
+        super().__init__()
